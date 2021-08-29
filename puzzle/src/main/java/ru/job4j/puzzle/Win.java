72744a93a410a4d4edf8c54870e22af85aca07da
11_pochta@bk.ru <11_pochta@bk.ru>
2021-08-29 16:17:24 +0600

    2. Сокобан. Тестовое задание. [#75283]
    Переписал методы. Убрал метод checkDiagonal. В методе check убрал 1 цикл for

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -4,35 +4,26 @@ public class Win {
     public static boolean check(int[][] board) {
         boolean rsl = false;
         for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                if(checkDiagonal(i, j, board) && (checkHorizontal(i, board) || checkVertical(j, board))){
-                    rsl = true;
-                    break;
-                }
+            if(board[i][i] == 1 && (checkHorizontal(board, i) || checkVertical(board, i))){
+                rsl = true;
+                break;
             }
         }
         return rsl;
     }
 
-    public static boolean checkDiagonal(int y, int x, int[][] array){
-        if((y == x || (y + x) == array.length) && array[y][x] == 1){
-            return true;
-        }
-        return false;
-    }
-
-    public static boolean checkHorizontal(int x, int[][] array){
+    public static boolean checkHorizontal(int[][] array, int row){
         for (int i = 0; i < array.length; i++) {
-            if(array[x][i] != 1){
+            if(array[row][i] != 1){
                 return false;
             }
         }
         return true;
     }
 
-    public static boolean checkVertical(int y, int[][] array){
+    public static boolean checkVertical(int[][] array, int column){
         for (int i = 0; i < array.length; i++) {
-            if(array[i][y] != 1){
+            if(array[i][column] != 1){
                 return false;
             }
         }
